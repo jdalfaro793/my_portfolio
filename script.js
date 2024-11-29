738dafcdf94eb2fe83f366557c6e704e1c7116a9
Josué Darío Alfaro <darioalfarounju@gmail.com>
2024-11-29 01:05:24 -0300

    fixes changes

--- script.js
+++ script.js
@@ -21,16 +21,12 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+
+        habilidades[0].classList.add("comunicacion");
+        habilidades[1].classList.add("trabajo");
+        habilidades[2].classList.add("creatividad");
+        habilidades[3].classList.add("dedicacion");
+        habilidades[4].classList.add("proyect");
     }
 }
 
